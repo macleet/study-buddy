aeaa347a55f91074aaaa9e851f15753e742c69a2
Mac Lee <seemin0506@gmail.com>
2024-05-30 02:02:25 -0700

    Add Tailwind config file.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,css,js,jsx}"],
+  theme: {
+    extend: {
+      blur: {
+        xxs: '1px',
+      }
+    },
+  },
+  plugins: [],
+}
+
