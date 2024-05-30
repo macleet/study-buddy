e3d7c887296596712ecc9f7c3a1ffe50edf29b79
Mac Lee <seemin0506@gmail.com>
2024-05-30 13:09:15 -0700

    Add skeleton code for Flashcard, Timer, and Video Recommendation features, for formatting.

---
+++ src/components/Flashcards/Flashcards.jsx
@@ -0,0 +1,7 @@
+export default () => {
+    return(
+        <div className='z-0 w-7/12 h-full flex flex-col overflow-auto' >
+            
+        </div>
+    );
+}
---
+++ src/components/Videos/Videos.jsx
@@ -0,0 +1,5 @@
+export default () => {
+    return(
+        <div className='z-0 w-7/12 h-full flex flex-col overflow-auto' ></div>
+    );
+}
